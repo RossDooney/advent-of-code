6c7874805bd8b008e608b42799b7125425cfce2f
Ross <rossdooney@gmail.com>
2023-12-04 13:28:23 +0000

    day 1 part two, start

--- day1/main.py
+++ day1/main.py
@@ -2,19 +2,21 @@ import os
 
 
 f = open("input.txt", "r")
+num_array = ["0", "zero", "1", "one", "2". "two", "3", "three", "4", "four", "5", "five"
+             "6", "six", "7", "seven", "8", "eight", "9", "nine", "10", "ten"]
 
 def main():
     total = 0
     for line in f:
         first = None
         last = None
-        for char in line:
-            if char.isnumeric():
-                last = char
+        index = 0
+        for num in num_array:
+            if num in line:
                 if first is None:
-                    first = char
-        line_total = str(first) + str(last)
-        total += int(line_total)
+                    first = num
+                
+
     print(total)
 
 main()
