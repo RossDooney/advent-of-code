a3dc1c5f441c70a611cba4aff04fe82e6a15991d
RossDooney <rossdooney@gmail.com>
2023-12-11 13:21:36 +0000

    day2 part 2

--- day2/main.py
+++ day2/main.py
@@ -11,16 +11,14 @@ def main():
         output_index = line.find(":")
         split_line = line[output_index+1:].split(";")
         for segment in split_line:
-            
-            if red > 12 or green > 13 or blue > 14:
-                break
+
             red_new, green_new, blue_new = 0, 0, 0
             for x in re.finditer("red", segment):
                 red_new = int(segment[x.start() - 3] + segment[x.start() - 2])
             for x in re.finditer("green", segment):
-                green = int(segment[x.start() - 3] + segment[x.start() - 2])
+                green_new = int(segment[x.start() - 3] + segment[x.start() - 2])
             for x in re.finditer("blue", segment):
-                blue = int(segment[x.start() - 3] + segment[x.start() - 2])
+                blue_new = int(segment[x.start() - 3] + segment[x.start() - 2])
 
             if red_new > red:
                 red = red_new
@@ -28,17 +26,9 @@ def main():
                 green = green_new
             if blue_new > blue:
                 blue = blue_new
-        print(red, green, blue)
-        if red > 12 or green > 13 or blue > 14:
-            continue
-        print(line[5:output_index].strip())
-        game_index = line[5:output_index].strip()
-        total += int(game_index)
-    print(total)
 
-
+        total += red * blue * green
+    
+    print(total)   
         
-
-
-
 main()
