847c0a0323ef8494ede1570d4a03076f4607bd1f
Ross <rossdooney@gmail.com>
2023-12-12 11:04:05 +0000

    day1 p2

--- day1/main.py
+++ day1/main.py
@@ -14,37 +14,36 @@ num_dict = {
     'seven': '7',
     'eight': '8',
     'nine': '9',
-    'zero': '0'
 }
 
 def main():
     total = 0
     number = 0
     for line in f:
-        start_index = -1
+        start_index = float("inf")
         start_word = ""
-        end_index = -1
+        end_index = float("-inf")
         end_word = ""
         for num in num_list:
             new_index = line.find(num)
             if new_index == -1:
                 continue
-            if start_word == "":
-                start_word = num
-                start_index = new_index
+
             if  start_index > new_index:
                 start_word = num
                 start_index = new_index
-            if end_word == "":
-                end_word = num
-                end_index = new_index
+
+            new_index = line.rfind(num)
             if end_index < new_index:
                 end_word = num
                 end_index = new_index
         
+
         start_word = convert_to_number(start_word)
         end_word = convert_to_number(end_word)
         number = start_word + end_word
+
+  
         total += int(number)
     print(total)
         
