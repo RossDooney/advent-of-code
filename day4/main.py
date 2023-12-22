29f5300a94f6a6f9321ad66dcc5a24f5003a8778
Ross <rossdooney@gmail.com>
2023-12-22 18:06:07 +0000

    day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -2,28 +2,58 @@ import os
 import re
 
 
-f = open("input.txt", "r")
+
 
 
 def main():
     part1()
+    part2()
 
 
 def part1():
+    f = open("input.txt", "r")
     total = 0
     for line in f:
         subTotal = 0
         winngNumbers, playerNumbers = parseLine(line)
         for playerNumber in playerNumbers.split():
             if playerNumber in winngNumbers.split():
-
                 if subTotal == 0:
                     subTotal += 1
                 else:
                     subTotal *= 2
+
         total += subTotal
-    print(total)
+    print("Part 1: ", total)
+    f.close()
             
+
+def part2():
+    f = open("input.txt", "r")
+    total = 0
+    card_index= [0]
+
+    for line in f:
+        i = 0
+        try:
+            card_index[0] += 1
+        except IndexError:
+            card_index.append(1)
+        total += card_index[0]
+        winngNumbers, playerNumbers = parseLine(line)
+        for playerNumber in playerNumbers.split():
+            if playerNumber in winngNumbers.split():
+                i += 1
+                try:
+                    card_index[i] += card_index[0] * 1
+                except IndexError:
+                    card_index.append(card_index[0] * 1)
+        
+        card_index.pop(0)
+
+
+    print("Part 2: ", total)
+    f.close()
 
 
 def parseLine(line):
