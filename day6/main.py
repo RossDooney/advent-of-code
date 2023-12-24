dfe8e19c52f5e33f019e2576c3a5a1535c851980
Ross <rossdooney@gmail.com>
2023-12-24 14:37:50 +0000

    day6 p1 bad solution

---
+++ day6/main.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+
+def main():
+    part1()
+
+def part1():
+    f = open("input.txt", "r")
+    inputTxt = f.readlines()
+    time = re.sub("\s\s+" , " ", inputTxt[0].split(":")[1]).split(" ")
+    distance = re.sub("\s\s+" , " ", inputTxt[1].split(":")[1]).split(" ")
+    total = 1
+
+    for x in range(1,len(time)):
+        index = 0
+        for y in range(int(time[x])):
+            if int(distance[x]) < (y * (int(time[x])-y)):
+                index += 1
+        total *= index
+
+    print(total)
+    
+    
+
+
+    
+main()
