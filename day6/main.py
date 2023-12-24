47e965ea2394463eab168e63612ef95e7d7237d7
Ross <rossdooney@gmail.com>
2023-12-24 22:24:19 +0000

    better search for index

--- day6/main.py
+++ day6/main.py
@@ -1,10 +1,14 @@
 import os
 import re
+import time
+
 
 
 def main():
     part1()
     part2()
+
+
 
 def part1():
     f = open("input.txt", "r")
@@ -12,6 +16,7 @@ def part1():
     time = re.sub("\s\s+" , " ", inputTxt[0].split(":")[1]).split(" ")
     distance = re.sub("\s\s+" , " ", inputTxt[1].split(":")[1]).split(" ")
     total = 1
+
 
     for x in range(1,len(time)):
         index = 0
@@ -31,9 +36,31 @@ def part2():
     time = int(''.join(re.sub("\s\s+" , " ", inputTxt[0].split(":")[1]).split(" ")))
     distance = int(''.join(re.sub("\s\s+" , " ", inputTxt[1].split(":")[1]).split(" ")))
     
-    for x in range(time):
-        if distance < (x * (time-x)):
-            print(time - (2 * x) + 1)
-            break
+    index = searchLowestIndex(time, distance, round(time/2), 1)
+    print(time - (2 * index) + 1)
 
+def searchLowestIndex(time, dist, index, lowest):
+    score = index * (time - index)
+    lowest_check = (index-1) * (time - (index - 1))
+
+
+    if dist < score and dist >= lowest_check:
+        return index
+    elif dist < score:
+        new_index = round((index + lowest)/2)
+        index = searchLowestIndex(time,dist,new_index,lowest)
+        return index
+    else:
+        new_index = (index + (time/2))/2
+        lowest = index
+        
+        index = searchLowestIndex(time,dist,new_index, lowest)
+
+    return index
+
+
+
+
+start_time = time.time()
 main()
+print("--- %s seconds ---" % (time.time() - start_time))
